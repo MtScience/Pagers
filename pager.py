1800e4a0fa9730b96bf68df4cb83102f36f3c534
MtScience <107070506+MtScience@users.noreply.github.com>
2023-04-04 12:27:18 +0300

    Update pager.py
    Updated to reflect the changes in Lua and Haskell versions

--- pager.py
+++ pager.py
@@ -1,6 +1,17 @@
+# The Python 3 version of the pager program. It basically mirrors the Lua version.
+
 from sys import argv, exit
 
 
+# The function asking the user for input data, if it is not supplied via CLI arguments
+def ask() -> tuple[int, int]:
+    pages: int = int(input("Please, enter the number of pages in the book: "))
+    sig_size: int = int(input("Please, enter the desired number of pages in a signature: "))
+
+    return pages, sig_size
+
+
+# Check inputs for correctness
 def check_inputs(size: int, sigs: float | int) -> None:
     if size not in (12, 16, 20):
         exit("Incorrect input data: signature size must be either 12, 16 or 20")
@@ -8,6 +19,7 @@ def check_inputs(size: int, sigs: float | int) -> None:
         exit("Incorrect input data: number of pages isn't divisible by signature size")
 
 
+# Do the job: calculate, where each page should go
 def calculate_pages(pages: int, size: int, sigs: int) -> list[tuple[list[str], list[str]]]:
     s: int = size // 2
     signatures: list[tuple[list[str], list[str]]] = []
@@ -25,6 +37,7 @@ def calculate_pages(pages: int, size: int, sigs: int) -> list[tuple[list[str], l
     return signatures
 
 
+# Output, obviously
 def print_signatures(sigs: list[tuple[list[str], list[str]]]) -> None:
     n = len(sigs)
     print(f"Total number of signatures is {n}.\n")
@@ -35,10 +48,15 @@ def print_signatures(sigs: list[tuple[list[str], list[str]]]) -> None:
         print(f"Side 2: {', '.join(s2)}\n")
 
 
+# The main block of the program
 if __name__ == "__main__":
     n_pages: int
     sig_size: int
-    n_pages, sig_size = int(argv[1]), int(argv[2])
+
+    if len(argv) >= 3:
+        n_pages, sig_size = int(argv[1]), int(argv[2])
+    else:
+        n_pages, sig_size = ask()
 
     n_sig: int | float = n_pages / sig_size
 
